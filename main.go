81f535d950d74f68329d61f12042a812ee647886
gurumee92 <gurumee92@gmail.com>
2020-07-07 19:12:37 +0900

    routing 작업 끝

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"net/http"
-
+	"github.com/gurumee92/devilog/handlers"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.GET("/", handlers.IndexPage)
+	e.GET("/login", handlers.LoginPage)
+	e.GET("/signup", handlers.SignupPage)
+	e.GET("/home/:username", handlers.UserHomePage)
 	e.Logger.Fatal(e.Start(":1323"))
 }
