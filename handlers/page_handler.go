3db7c0440cd2000b8fc01526a19175317dd2c150
gurumee92 <gurumee92@gmail.com>
2020-07-07 19:09:08 +0900

    Add Functions

--- handlers/page_handler.go
+++ handlers/page_handler.go
@@ -10,3 +10,19 @@ import (
 func IndexPage(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello Echo")
 }
+
+// LoginPage is rendering Login Page
+func LoginPage(c echo.Context) error {
+	return c.String(http.StatusOK, "Login")
+}
+
+// SignupPage is rendering Signup Page
+func SignupPage(c echo.Context) error {
+	return c.String(http.StatusOK, "Signup")
+}
+
+// UserHomePage is rendering User's Home Page
+func UserHomePage(c echo.Context) error {
+	username := c.Param("username")
+	return c.String(http.StatusOK, username)
+}
--- handlers/page_handler_test.go
+++ handlers/page_handler_test.go
@@ -22,3 +22,55 @@ func TestIndexPage(t *testing.T) {
 		assert.Equal(t, "Hello Echo", rec.Body.String())
 	}
 }
+
+func TestLoginPage(t *testing.T) {
+	// Setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/login", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	// Assertions
+	if assert.NoError(t, LoginPage(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, "Login", rec.Body.String())
+	}
+}
+
+func TestSignupPage(t *testing.T) {
+	// Setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/signup", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	// Assertions
+	if assert.NoError(t, SignupPage(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, "Signup", rec.Body.String())
+	}
+}
+
+func testUserHomePage(t *testing.T, username string) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/home/:username", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/home/:username")
+	c.SetParamNames("username")
+	c.SetParamValues(username)
+
+	// Assertions
+	if assert.NoError(t, UserHomePage(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, username, rec.Body.String())
+	}
+}
+
+func TestUserHomePage(t *testing.T) {
+	usernames := []string{"test1", "test2"}
+
+	for _, username := range usernames {
+		testUserHomePage(t, username)
+	}
+}
