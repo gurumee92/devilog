40f564da921475aca339f2d93915d8dd79bf5e4c
gurumee92 <gurumee92@gmail.com>
2020-07-07 18:02:53 +0900

    Add page_handler_test.go

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/gurumee92/devilog
 
 go 1.14
 
-require github.com/labstack/echo/v4 v4.1.16
+require (
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/echo/v4 v4.1.16
+	github.com/stretchr/testify v1.4.0
+)
---
+++ handlers/page_handler_test.go
@@ -0,0 +1,24 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIndexPage(t *testing.T) {
+	// Setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	// Assertions
+	if assert.NoError(t, IndexPage(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, "Hello Echo", rec.Body.String())
+	}
+}
