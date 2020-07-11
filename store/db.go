f0f230ee0d8e4353fe216c5d21c91fee05dea87c
gurumee92 <gurumee92@gmail.com>
2020-07-11 14:00:10 +0900

    Add store/db.go

---
+++ store/db.go
@@ -0,0 +1,55 @@
+package store
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres" // init postgres
+	_ "github.com/jinzhu/gorm/dialects/sqlite"   // init sqlite
+)
+
+// GetDB is get db instance
+func GetDB() *gorm.DB {
+	host := os.Getenv("DATABASE_HOST")
+	port := os.Getenv("DATABASE_PORT")
+	user := os.Getenv("DATABASE_USER")
+	pass := os.Getenv("DATABASE_PASS")
+	name := os.Getenv("DATABASE_NAME")
+	url := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s", host, port, user, name, pass)
+	db, err := gorm.Open("postgres", url)
+
+	if err != nil {
+		fmt.Println("storage err: ", err)
+	}
+
+	db.DB().SetMaxIdleConns(3)
+	db.LogMode(true)
+	return db
+}
+
+// GetTestDB is get db instance for test
+func GetTestDB() *gorm.DB {
+	db, err := gorm.Open("sqlite3", "./test.db")
+
+	if err != nil {
+		fmt.Println("storage err: ", err)
+	}
+
+	db.DB().SetMaxIdleConns(3)
+	db.LogMode(true)
+	return db
+}
+
+// DropTestDB is drop test db
+func DropTestDB() error {
+	if err := os.Remove("./test.db"); err != nil {
+		return err
+	}
+	return nil
+}
+
+// AutoMigrate is migrate code and databaseßß
+func AutoMigrate(db *gorm.DB) {
+	db.AutoMigrate()
+}
