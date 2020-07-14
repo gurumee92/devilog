9b77a770164ea54bd86fb1fedee0523cd928bed4
gurumee92 <gurumee92@gmail.com>
2020-07-14 19:59:31 +0900

    Add Config

---
+++ config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"log"
+	"os"
+	"strings"
+)
+
+// Config is
+type Config struct {
+	ApplicationPath string
+	DatabaseDialect string
+	DatabaseURL     string
+}
+
+// GetConfig is
+func GetConfig() Config {
+	return Config{
+		ApplicationPath: os.Getenv("APPLICATION_PATH"),
+		DatabaseDialect: os.Getenv("DATABASE_DIALECT"),
+		DatabaseURL:     os.Getenv("DATABASE_URL"),
+	}
+}
+
+// GetTestConfig is
+func GetTestConfig() Config {
+	wd, err := os.Getwd()
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	s := strings.Split(wd, "/")
+	path := strings.Join(s[:len(s)-1], "/")
+
+	return Config{
+		ApplicationPath: path,
+		DatabaseDialect: "sqlite3",
+		DatabaseURL:     "./test.db",
+	}
+}
