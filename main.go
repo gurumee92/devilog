b8e641ec28795e933f43c6e7df956339102e8d61
gurumee92 <gurumee92@gmail.com>
2020-07-14 20:35:39 +0900

    Code 병합 config

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"github.com/gurumee92/devilog/config"
 	"github.com/gurumee92/devilog/handler"
 	"github.com/gurumee92/devilog/router"
 	"github.com/gurumee92/devilog/store"
 )
 
 func main() {
+	c := config.GetConfig()
 	// database
-	db := store.GetDB()
+	db := store.GetDB(c)
 	defer db.Close()
 	store.AutoMigrate(db)
 	postStore := store.NewPostStore(db)
