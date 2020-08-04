8f8bbfc5bf7b97af8dbbd25d4e5e9484bffc4da2
gurumee92 <gurumee92@gmail.com>
2020-08-04 18:38:57 +0900

    Add model Account

---
+++ model/user.go
@@ -0,0 +1,12 @@
+package model
+
+import "github.com/jinzhu/gorm"
+
+// Account is Entity account
+type Account struct {
+	gorm.Model
+	Email    string `gorm:"not null"`
+	Password string `gorm:"not null"`
+	Username string `gorm:"not null"`
+	Picture  string `gorm:"not null"`
+}
